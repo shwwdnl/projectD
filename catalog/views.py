262ec420721709f639b472d9ecaf0f3b2bd6e167
shwwdnl <shwwdnl@gmail.com>
2023-11-27 17:16:54 +0500

    add file

---
+++ catalog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'main/home.html')
+
+def contacts(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'{name} ({email}): {message}')
+    return render(request, 'main/contacts.html')
