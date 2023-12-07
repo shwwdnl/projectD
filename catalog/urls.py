17350f9c57f09c3e05e2ff3dab57e3b8012b19ac
shwwdnl <shwwdnl@gmail.com>
2023-12-07 18:04:46 +0500

    add file

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from catalog.views import home, contacts, product
+from catalog.apps import CatalogConfig
+from catalog.views import ContactsView, ProductListView, ProductDetailView
 
-
+app_name = CatalogConfig.name
 urlpatterns = [
-    path('', home),
-    path('contacts/', contacts),
-    path('product/', product),
+    path('', ProductListView.as_view(), name='home'),
+    path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,26 +1,32 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, TemplateView
 
 from catalog.models import Product
 
-
-def home(request):
-    catalog_list = Product.objects.all()
-    context = {
-        'object_list' : catalog_list,
+class ProductListView(ListView):
+    model = Product
+    extra_context = {
+        'title': 'Главная страница',
     }
-    return render(request, 'main/home.html', context)
 
-def product(request):
-    product_list = Product.objects.all()
-    context = {
-        'object_list': product_list,
+
+class ProductDetailView(DetailView):
+    model = Product
+    extra_context = {
+        'title': 'Товар',
     }
-    return render(request, 'main/product.html', context)
 
-def contacts(request):
-    if request.method == "POST":
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-        print(f'{name} ({email}): {message}')
-    return render(request, 'main/contacts.html')
+
+class ContactsView(TemplateView):
+    template_name = 'catalog/contacts.html'
+    extra_context = {
+        'title': 'Контакты',
+    }
+
+    def contacts(self, request, *args, **kwargs):
+        if request.method == "POST":
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            message = request.POST.get('message')
+            print(f'{name} ({email}): {message}')
+        return render(request, 'catalog/contacts.html', self.extra_context)
